a4c7f8a2801a066704bc2d0693b1ff57f61fdf4e
Shahed70 <58668503+Shahed70@users.noreply.github.com>
2021-06-09 22:22:41 +0600

    problem fixed

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,10 @@ import Dashboard from './components/Dashboard/Dashboard'
 
 
 const App = () => {
-  const [route, setRoute] = useState<any>('/home')
-  const headerContent = () => {
-    let adminRoute;
-    switch (route) {
-      case "/home":
-        return adminRoute = 'Home Component';
-      case '/work':
-        return adminRoute = 'Work Component'
-      default:
-        return adminRoute = 'Home component'
-    }
-  }
 
   return (
       <div>
-        <Dashboard headerContent={headerContent} />
+        <Dashboard/>
       </div>
   )
 }
